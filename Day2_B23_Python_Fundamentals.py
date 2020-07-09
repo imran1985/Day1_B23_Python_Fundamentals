2d83c43fe31f266eabc9bb040cb14a726d58b5ba
imran1985 <68021179+imran1985@users.noreply.github.com>
2020-07-09 17:41:53 +0530

    Add files via upload

---
+++ Day2_B23_Python_Fundamentals.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+first_name = 'imran'
+print (first_name)
+
+
+# In[2]:
+
+
+firstname = 'imran'
+print (firstname)
+
+
+# In[3]:
+
+
+1name = 'imran'
+print (1name)
+
+
+# In[4]:
+
+
+name@ = 'imran'
+print (name@)
+
+
+# In[6]:
+
+
+name1 = 'imran1'
+print(name1)
+
+
+# In[7]:
+
+
+name2 = "imran2"
+print (name2)
+
+
+# In[9]:
+
+
+name3 = """imran3"""
+print (name3)
+
+
+# In[10]:
+
+
+full_name = 'imran ansari'
+print(full_name)
+
+
+# In[11]:
+
+
+print(full_name.title())
+
+
+# In[12]:
+
+
+print(full_name.upper())
+
+
+# In[ ]:
+
+
+
+
