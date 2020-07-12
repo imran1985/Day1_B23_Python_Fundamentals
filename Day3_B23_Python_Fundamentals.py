39dc0e3f08433151ed5383103e43a732aebbef41
imran1985 <68021179+imran1985@users.noreply.github.com>
2020-07-12 22:25:00 +0530

    Add files via upload

---
+++ Day3_B23_Python_Fundamentals.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+first_name = 'imran'
+
+last_name = 'ansari'
+
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+
+
+# In[ ]:
+
+
+# enhancement of the code
+
+
+# In[5]:
+
+
+print(full_name.title())
+
+
+# In[ ]:
+
+
+# custom line
+
+
+# In[7]:
+
+
+print(f"keep up the good work, {full_name.title()}")
+
+
+# In[ ]:
+
+
+# adding white space to the string
+
+
+# In[8]:
+
+
+print('fav_language : pythonjavacswiftc++')
+
+
+# In[10]:
+
+
+print('fav_language : \npython\njava\nc\nswift\nc++')
+
+
+# In[14]:
+
+
+print('fav_language : \n\tpython\n\tjava\n\tc\n\tswift\n\tc++')
+
+
+# In[ ]:
+
+
+# Removing whitespace to string
+
+
+# In[16]:
+
+
+fav_lang = 'python '
+
+print(fav_lang)
+
+
+# In[18]:
+
+
+fav_lang.rstrip()
+
+
+# In[19]:
+
+
+fav_lang = ' python'
+
+print(fav_lang)
+
+
+# In[20]:
+
+
+fav_lang.lstrip()
+
+
+# In[21]:
+
+
+fav_lang = ' python '
+
+print (fav_lang)
+
+
+# In[22]:
+
+
+fav_lang.strip()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
